5ae7c550cf63630cc1137ac3c21ec76fdc55f099
Your Name <you@example.com>
2025-09-11 20:22:54 +0400

    fix: vercel build (i18n, scripts, tsconfig exclude)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['ar', 'en', 'fr', 'hi', 'fa', 'ur'],
-    defaultLocale: 'ar',
-    localeDetection: true,
-  },
-  // إعدادات أخرى إذا لزم الأمر
+    locales: ["ar", "en", "fr", "hi", "fa", "ur"],
+    defaultLocale: "ar",
+    localeDetection: false
+  }
 };
 
 module.exports = nextConfig;
