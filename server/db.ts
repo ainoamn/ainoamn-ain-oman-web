264caac775c1c9970d82e3087c414c946182826d
Your Name <you@example.com>
2025-09-21 23:18:02 +0400

    Auto commit: 2025-09-21 23:18

---
+++ server/db.ts
@@ -0,0 +1,10 @@
+// server/db.ts
+export const getTask = () => {};
+export const patchTask = () => {};
+export type Task = any;
+export const appendThread = () => {};
+export const addParticipant = () => {};
+export const addAttachment = () => {};
+export const getAttachmentPath = () => {};
+export const removeAttachment = () => {};
+export const upsertTask = () => {};
